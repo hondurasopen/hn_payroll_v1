94beed5f85510f738b15f33d21768d47c05a3701
rodrice <cesar.rodriguez@davidoff.com>
2020-06-14 19:56:59 -0600

    Special version for hn payroll

--- __openerp__.py
+++ __openerp__.py
@@ -7,7 +7,7 @@
         Honduras Payroll  Version Especial
     """,          
     'version': '1.0',
-    'depends': ['base', 'hr', "hr_contract"],
+    'depends': ['base', 'hr', "hr_payroll", "hr_contract"],
     'data': [
         "wizard/set_structure_contract.xml",
     	"views/hr_payslip_inh.xml",
