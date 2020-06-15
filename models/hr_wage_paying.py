7963aa8c3b524c4b1937fbb1c3f8b0ae2187d60a
rodrice <cesar.rodriguez@davidoff.com>
2020-06-15 10:09:02 -0600

    Special version for hn payroll

--- models/hr_wage_paying.py
+++ models/hr_wage_paying.py
@@ -34,16 +34,13 @@ class HrPrePayroll(models.Model):
         	line_obj.create(vals)
 
 
-
-
-
 class HrPrePayroll(models.Model):
     _name = 'hr.wage.paying.line'
 
     @api.one
     @api.depends("gross_wage", "loan_fee", "saving_fee", "amount_isr", "amount_ipv", "another_incomes")
     def _compute_amount(self):
-    	self.gross_wage = self.wage + self.another_incomes 
+    	self.gross_wage = self.wage + self.other_incomes 
     	self.amount_deduction = self.loan_fee + self.saving_fee + self.amount_isr + self.amount_ipv + self.other_deduction
     	self.amount_net = self.gross_wage - self.amount_deduction
 
