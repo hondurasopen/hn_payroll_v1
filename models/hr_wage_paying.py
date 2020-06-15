3a2d0a40774bcac6a8c757a29f57cf5a6df5859b
rodrice <cesar.rodriguez@davidoff.com>
2020-06-15 10:07:30 -0600

    Special version for hn payroll

--- models/hr_wage_paying.py
+++ models/hr_wage_paying.py
@@ -40,15 +40,25 @@ class HrPrePayroll(models.Model):
 class HrPrePayroll(models.Model):
     _name = 'hr.wage.paying.line'
 
+    @api.one
+    @api.depends("gross_wage", "loan_fee", "saving_fee", "amount_isr", "amount_ipv", "another_incomes")
+    def _compute_amount(self):
+    	self.gross_wage = self.wage + self.another_incomes 
+    	self.amount_deduction = self.loan_fee + self.saving_fee + self.amount_isr + self.amount_ipv + self.other_deduction
+    	self.amount_net = self.gross_wage - self.amount_deduction
+
     parent_id = fields.Many2one("hr.wage.paying", "Pago")
-    employee_id = fields.Many2one("hr.employee", "empleado")
+    employee_id = fields.Many2one("hr.employee", "Empleado")
     wage = fields.Float("Salario")
-    gross_wage = fields.Float("Salario bruto")
     loan_fee = fields.Float("Cuota de préstamo")
     saving_fee = fields.Float("Aporte Cooperativa")
     amount_isr = fields.Float("ISR")
     amount_ipv = fields.Float("Impuesto Vecinal")
-    another_deduction = fields.Float("Otra deducción")
-    another_incomes = fields.Float("Otros ingresos")
-    amount_deduction = fields.Float("Total de deducciones")
-    amount_net = fields.Float("Neto a pagar")
+    other_deduction = fields.Float("Otra deducción")
+    other_incomes = fields.Float("Otros ingresos")
+    gross_wage = fields.Float("Salario bruto", compute='_compute_amount')
+    amount_deduction = fields.Float("Total de deducciones", compute='_compute_amount')
+    amount_net = fields.Float("Neto a pagar", compute='_compute_amount')
+
+    
+
