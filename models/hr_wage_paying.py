85a13a5c027f283096e2564680285c0156b1df5a
rodrice <cesar.rodriguez@davidoff.com>
2020-06-16 16:51:38 -0600

    Generate journal entry

--- models/hr_wage_paying.py
+++ models/hr_wage_paying.py
@@ -60,7 +60,7 @@ class HrPrePayroll(models.Model):
                     'concept_id': concept.id,
                 }
                 if concept.concept == 'gross':
-                    vals["amount"] = self.gross_total
+                    vals["amount"] = 100 ##sself.gross_total
                 if concept.concept == 'net':
                     vals["amount"] = self.net_total
                 if concept.concept == 'loan':
