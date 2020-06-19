a211412e078550c019626451aa7ee4f2ed69f179
rodrice <cesar.rodriguez@davidoff.com>
2020-06-18 19:36:40 -0600

    Generate historical

--- models/contract.py
+++ models/contract.py
@@ -7,6 +7,4 @@ class DoffHistorialSalarios(models.Model):
     #DEDUCCIONES
     allowences_ids = fields.One2many("hr.contract.deduction.allowance", "contract_id", "Otros Beneficios y deducciones")
     historical_ids = fields.One2many("hr.historial.contract", "contract_id", "Historial")
-
-
-
+    historical_wage_ids = fields.One2many("hr.historical.wage", "contract_id", "Salarios Brutos")
